650b42003e477521abb649d692d4710c4c8bc04a
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-03-17 20:45:22 +0000

    Determine whether the number is (+) or (-)

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,14 +19,14 @@ int main(void)
 }
 	if (n > 0)
 	{
-		printf("The number is positive\n");
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("The number is negative\n");
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("The number is zero\n");
+		printf("%d is zero\n", n);
 	}
 	return (0);
