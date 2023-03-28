8c306b3099a9f68cb72f38a0a0a8c945ef82ea2b
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-03-28 01:53:13 +0000

    Copies string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strcpy - copies the string to the buffer
@@ -11,8 +10,6 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	if (dest == NULL)
-		return (NULL);
 	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
 	return (dest);
