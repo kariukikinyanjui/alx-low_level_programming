5138c8c0dcb56bf45250ae7b9e3c93bd30be8dcf
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-03-31 01:04:02 +0000

    Encodes a string using rot13

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,20 +3,23 @@
  * _strncat - concatenate two strings
  * @dest: destination string
  * @src: source string
- * @n: bytes
+ * @n: number of bytes
  * Return: destination string
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int destlen = 0;
-	int srclen = 0;
 	int i;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		destlen++;
-	for (i = 0; src[i] != '\0'; i++)
-		srclen++;
-	for (i = 0; i < n; i++)
-		dest[destlen + i] = src[i];
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
