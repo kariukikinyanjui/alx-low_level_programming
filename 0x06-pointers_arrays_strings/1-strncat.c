fbddc563f4f4a7f50b2044052f3eb31d3999ddf1
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-03-31 01:32:26 +0000

    Concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,18 +8,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int destlen = 0;
+	int srclen = 0;
 	int i;
 
-	i = 0;
-	while (i < n && src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
+	for (i = 0; dest[i] != '\0'; i++)
+		destlen++;
+	for (i = 0; src[i] != '\0'; i++)
+		srclen++;
+	for (i = 0; i < n; i++)
+		dest[destlen + i] = src[i];
 	return (dest);
 }
