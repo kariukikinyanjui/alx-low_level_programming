69996079f1c65e7fabe855e6856448b3290754cc
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-04-03 23:42:52 +0000

    Prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer to a two-dimensional character array representing the chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int b;
+	int c;
+
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			printf("%c ", a[b][c]);
+		}
+
+		printf("\n");
+	}
+}
