911a139fe6646ed812b46287cf941be62863bdc8
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-06-07 05:24:28 +0000

    A function that prints a string, similar to puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string followed by a newline
+ * @s: character pointer
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	putchar('\n');
+	return;
+	}
+	else
+	{
+	putchar(*s);
+
+	_puts_recursion(s + 1);
+	}
+}
