912bc19a6d643288f0a915cbec770561e92e022e
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-06-21 18:46:40 +0000

    Functions that perform simple arithmetics

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "function_pointers.h"
+
+/**
+ * op_add - a function that returns the sum of a and b
+ * @a: function parameter 1
+ * @b: function parameter 2
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - a function that returns the difference of a and b
+ * @a: function parameter 1
+ * @b: function parameter 2
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - a function that returns the product of a by b
+ * @a: function parameter 1
+ * @b: function parameter 2
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - a function that returns the resultof the division of a by b
+ * @a: function parameter 1
+ * @b: function parameter 2
+ * Return: result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - a function that returns the remainder of the divison of a by b
+ * @a: function parameter 1
+ * @b: function parameter 2
+ * Return: the remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
