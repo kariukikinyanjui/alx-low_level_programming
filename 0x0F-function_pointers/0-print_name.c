bef62710f1b92a732031e9f804a41d4c8bfe92e6
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-06-21 14:21:54 +0000

    A function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,14 @@
  * print_name - function that takes a name as input
  * @name: pointer to a character array representing the name
  * @f: function pointer
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL)
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
 	{
 		(*f)(name);
 	}
