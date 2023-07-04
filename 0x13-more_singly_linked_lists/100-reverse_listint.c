7688d6b66def5830da70ef699fad30e1fd8aa685
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-07-04 23:13:08 +0000

    Declared to variables instead of three

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,14 +9,13 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *previous = NULL;
 	listint_t *current = *head;
-	listint_t *next = NULL;
 
 	while (current != NULL)
 	{
-		next = current->next;
+		*head = current->next;
 		current->next = previous;
 		previous = current;
-		current = next;
+		current = *head;
 	}
 
 	*head = previous;
