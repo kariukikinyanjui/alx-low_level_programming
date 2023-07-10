f3f75c059d8d6ac4fa04b4d4c2925d5ee13ee571
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-07-10 20:50:20 +0000

    Updated header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#define BUFF_SIZE 1024
+#define BUFF_SIZE 4096
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <elf.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
