a1d10cc00455ea707f1422624705aee9f0786ad7
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-07-11 00:28:16 +0000

    Fixed typos causing errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * appen_text_to_file - a function that appends text at the
+ * append_text_to_file - a function that appends text at the
  * end of a file
  * @filename: name of the file
  * @text_content: NULL terminated string to add at the end of the file
  * Return: 1 on success -1 on failure
  */
-int appent_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	FILE *file;
 	size_t length;
