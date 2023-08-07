2c7dce61304e9839a95ed89e6b6dc357217159cb
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-08-07 13:03:41 +0000

    Updated header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,6 +13,8 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *create_buffer(char *file);
+void close_file(int fd);
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
